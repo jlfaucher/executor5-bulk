1c73f2ae9c8c0a1bfbc806580ebe65078c5f5703
wdashley <wdashley@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2006-11-20 17:36:22 +0000

    ArtifactID: None Comment: Fixes for the Linux auto tools files.
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@61 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- interpreter/trunk/make.bat
+++ interpreter/trunk/make.bat
@@ -1,11 +1,12 @@
 @echo off
 rem
-rem There are 3 ways of running this command:
+rem There are 2 ways of running this command:
 rem
 rem      make
-rem      make debug
 rem      make clean
 rem
 rem Anything else will cause problems.
+rem
+rem Note that debugging is set in the Nmake.inc file, not here.
 rem
 @nmake /F Makefile.win %1
