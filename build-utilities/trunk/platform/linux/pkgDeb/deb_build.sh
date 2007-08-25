b256978423f634d2fe3d42e8114727e48d5c9bd0
miesfeld <miesfeld@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2007-08-25 17:23:50 +0000

    deb_build.sh A synatx in this script is bash specific.  Since the script itself is Linux specific, specify bash in the hash bang line.
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@712 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- build-utilities/trunk/platform/linux/pkgDeb/deb_build.sh
+++ build-utilities/trunk/platform/linux/pkgDeb/deb_build.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 #/*----------------------------------------------------------------------------*/
 #/*                                                                            */
 #/* Copyright (c) 2007 Rexx Language Association. All rights reserved.         */
