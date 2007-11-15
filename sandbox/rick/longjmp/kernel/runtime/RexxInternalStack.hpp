b4b1d9fe200013b6ca1bef4eea4edd8f478dd80f
bigrixx <bigrixx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2007-11-15 01:25:36 +0000

    Oh, I love working with different compilers....
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@1245 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- sandbox/rick/longjmp/kernel/runtime/RexxInternalStack.hpp
+++ sandbox/rick/longjmp/kernel/runtime/RexxInternalStack.hpp
@@ -48,6 +48,7 @@
 class RexxInternalStack : public RexxInternalObject {
  public:
   inline void *operator new(size_t size, void *ptr) { return ptr; }
+  inline void  operator delete(void *) { ; }
   inline void  operator delete(void *, void *) { ; }
 
   RexxInternalStack() { ; }
