71d8698cd2b7b6dd0b91ed94db8830eb78ca0245
bigrixx <bigrixx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2007-11-15 01:08:13 +0000

    Linux build fix.
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@1242 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- sandbox/rick/longjmp/kernel/runtime/RexxActivationStack.hpp
+++ sandbox/rick/longjmp/kernel/runtime/RexxActivationStack.hpp
@@ -49,6 +49,7 @@
 class RexxActivationFrameBuffer : public RexxInternalObject {
     public:
      inline void *operator new(size_t size, void *ptr) { return ptr;};
+     inline void  operator delete(void *) { ; }
      inline void  operator delete(void *, void *) { ; }
 
      RexxActivationFrameBuffer() { ; }
