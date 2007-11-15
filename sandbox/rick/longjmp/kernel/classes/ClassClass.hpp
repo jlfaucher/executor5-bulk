021e27baa0cd84103c1e326e05c6c4a10dcc67b4
bigrixx <bigrixx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2007-11-15 00:34:00 +0000

    Fix Linux build problem
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@1235 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- sandbox/rick/longjmp/kernel/classes/ClassClass.hpp
+++ sandbox/rick/longjmp/kernel/classes/ClassClass.hpp
@@ -49,6 +49,7 @@
   public:
    void *operator new(size_t, size_t, const char *, RexxBehaviour *, RexxBehaviour *);
    inline void *operator new(size_t size, void *ptr) {return ptr;};
+   inline void operator delete(void *) { }
    inline void operator delete(void *, void *) { }
    inline void operator delete(void *, size_t, const char *, RexxBehaviour *, RexxBehaviour *) { }
 
