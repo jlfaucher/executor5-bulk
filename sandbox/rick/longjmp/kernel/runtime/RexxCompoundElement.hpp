06878a76cbd7d6b133903272ed72744a941ce135
bigrixx <bigrixx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2007-11-15 01:14:29 +0000

    Linux build fix.
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@1243 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- sandbox/rick/longjmp/kernel/runtime/RexxCompoundElement.hpp
+++ sandbox/rick/longjmp/kernel/runtime/RexxCompoundElement.hpp
@@ -52,6 +52,7 @@ class RexxCompoundElement : public RexxVariable {
 
  public:
   inline void *operator new(size_t size, void *ptr) { return ptr; };
+  inline void  operator delete(void *) { ; }
   inline void  operator delete(void *, void *) { ; }
 
   inline RexxCompoundElement(RESTORETYPE restoreType) { ; };
