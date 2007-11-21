1d9591dcff4da8acaf11ed5bf83b6d2587c0f7e0
antiguru <antiguru@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2007-11-21 15:13:55 +0000

    A new class loader release. Code cleanup and ongoing development.
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@1285 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

---
+++ sandbox/moritz/classloader/root/test.rex
@@ -0,0 +1,6 @@
+Say "Message from test.rex"
+
+return .TestData
+::CLASS TestData SUBCLASS PackageData
+::METHOD Classes
+    return .array~new
--- sandbox/moritz/classloader/test.cmd
+++ sandbox/moritz/classloader/test.cmd
@@ -3,7 +3,8 @@
 
 -- Testcase for ClassLoader
 
-
+say .package~test
+say .package~asdlfkj
 a = .Test~new
 say a
 b = .otherclass
@@ -11,5 +12,8 @@ say b
 if a~class = .test then say "hooray, it worked!"
 
 ::REQUIRES "test~testpackage(someclass as somecl, otherclass)"
+::REQUIRES "test"
+-- uncomment to test errors in class loading
+--::REQUIRES "test~test"
 
 ::CLASS Test SUBCLASS SomeCl
