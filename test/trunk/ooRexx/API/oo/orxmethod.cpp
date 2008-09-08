cf54fcb572c23858a611ec61ea8448a77882f505
wdashley <wdashley@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2008-09-08 18:43:50 +0000

    ArtifactID: None Comment: Initial Start of the API unit tests. This is just a prototype for now. More later after some discussion.
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@3237 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

---
+++ test/trunk/ooRexx/API/oo/orxmethod.cpp
@@ -0,0 +1,190 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/* Copyright (c) 2008 Rexx Language Association. All rights reserved.         */
+/*                                                                            */
+/* This program and the accompanying materials are made available under       */
+/* the terms of the Common Public License v1.0 which accompanies this         */
+/* distribution. A copy is also available at the following address:           */
+/* http://www.oorexx.org/license.html                                         */
+/*                                                                            */
+/* Redistribution and use in source and binary forms, with or                 */
+/* without modification, are permitted provided that the following            */
+/* conditions are met:                                                        */
+/*                                                                            */
+/* Redistributions of source code must retain the above copyright             */
+/* notice, this list of conditions and the following disclaimer.              */
+/* Redistributions in binary form must reproduce the above copyright          */
+/* notice, this list of conditions and the following disclaimer in            */
+/* the documentation and/or other materials provided with the distribution.   */
+/*                                                                            */
+/* Neither the name of Rexx Language Association nor the names                */
+/* of its contributors may be used to endorse or promote products             */
+/* derived from this software without specific prior written permission.      */
+/*                                                                            */
+/* THIS SOFTWARE IS PROVIDED BY THE COPYright HOLDERS AND CONTRIBUTORS        */
+/* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT          */
+/* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS          */
+/* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYright   */
+/* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,      */
+/* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED   */
+/* TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,        */
+/* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY     */
+/* OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    */
+/* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS         */
+/* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.               */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+
+RexxMethod0(int,                       // Return type
+            TstArgs000)                // Object_method name
+{
+#include "tstargs000.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs001,                // Object_method name
+            int, arg1)                 // Argument
+{
+#include "tstargs001.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs002,                // Object_method name
+            int, arg1,                 // Argument
+            int, arg2)                 // Argument
+{
+#include "tstargs002.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs003,                // Object_method name
+            int, arg1)                 // Argument
+            int, arg2,                 // Argument
+            int, arg3)                 // Argument
+{
+#include "tstargs003.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs004,                // Object_method name
+            int, arg1)                 // Argument
+            int, arg2,                 // Argument
+            int, arg3,                 // Argument
+            int, arg4)                 // Argument
+{
+#include "tstargs004.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs005,                // Object_method name
+            int, arg1)                 // Argument
+            int, arg2,                 // Argument
+            int, arg3,                 // Argument
+            int, arg4,                 // Argument
+            int, arg5)                 // Argument
+{
+#include "tstargs005.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs006,                // Object_method name
+            int, arg1)                 // Argument
+            int, arg2,                 // Argument
+            int, arg3,                 // Argument
+            int, arg4,                 // Argument
+            int, arg5,                 // Argument
+            int, arg6)                 // Argument
+{
+#include "tstargs006.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs007,                // Object_method name
+            int, arg1)                 // Argument
+            int, arg2,                 // Argument
+            int, arg3,                 // Argument
+            int, arg4,                 // Argument
+            int, arg5,                 // Argument
+            int, arg6,                 // Argument
+            int, arg7)                 // Argument
+{
+#include "tstargs007.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs008,                // Object_method name
+            int, arg1)                 // Argument
+            int, arg2,                 // Argument
+            int, arg3,                 // Argument
+            int, arg4,                 // Argument
+            int, arg5,                 // Argument
+            int, arg6,                 // Argument
+            int, arg7,                 // Argument
+            int, arg8)                 // Argument
+{
+#include "tstargs008.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs009,                // Object_method name
+            int, arg1)                 // Argument
+            int, arg2,                 // Argument
+            int, arg3,                 // Argument
+            int, arg4,                 // Argument
+            int, arg5,                 // Argument
+            int, arg6,                 // Argument
+            int, arg7,                 // Argument
+            int, arg8,                 // Argument
+            int, arg9)                 // Argument
+{
+#include "tstargs009.cpp"
+}
+
+RexxMethod1(int,                       // Return type
+            TstArgs010,                // Object_method name
+            int, arg1)                 // Argument
+            int, arg2,                 // Argument
+            int, arg3,                 // Argument
+            int, arg4,                 // Argument
+            int, arg5,                 // Argument
+            int, arg6,                 // Argument
+            int, arg7,                 // Argument
+            int, arg8,                 // Argument
+            int, arg9,                 // Argument
+            int, arg10)                // Argument
+{
+#include "tstargs010.cpp"
+}
+
+
+RexxMethodEntry orxtest_methods[] = {
+    REXX_METHOD(TstArgs000, TstArgs000),
+    REXX_METHOD(TstArgs001, TstArgs001),
+    REXX_METHOD(TstArgs002, TstArgs002),
+    REXX_METHOD(TstArgs003, TstArgs003),
+    REXX_METHOD(TstArgs004, TstArgs004),
+    REXX_METHOD(TstArgs005, TstArgs005),
+    REXX_METHOD(TstArgs006, TstArgs006),
+    REXX_METHOD(TstArgs007, TstArgs007),
+    REXX_METHOD(TstArgs008, TstArgs008),
+    REXX_METHOD(TstArgs009, TstArgs009),
+    REXX_METHOD(TstArgs010, TstArgs010),
+    REXX_LAST_METHOD()
+};
+
+
+RexxPackageEntry UnitTest_package_entry = {
+    STANDARD_PACKAGE_HEADER
+    REXX_INTERPRETER_4_0_0,              // anything after 4.0.0 will work
+    "UnitTest",                          // name of the package
+    VERSTRING("1.0.0").                  // package information
+    NULL,                                // no load/unload functions
+    NULL,
+    NULL,                                // the exported routines
+    orxtest_methods                      // the exported methods
+};
+
+// package loading stub.
+OOREXX_GET_PACKAGE(UnitTest);
+
