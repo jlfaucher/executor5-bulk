1c5a4640dd2aff1aa3ebf351eece9accf7b56fd2
antiguru <antiguru@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2010-04-08 13:02:38 +0000

    Add some initial work for a workbench, based on Eclipse. This is an (incomplete) editor for ooRexx files.
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@5769 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

---
+++ incubator/workbench/org.oorexx.workbench.edit/src/org/oorexx/workbench/validation/RexxJavaValidator.java
@@ -0,0 +1,13 @@
+package org.oorexx.workbench.validation;
+ 
+
+public class RexxJavaValidator extends AbstractRexxJavaValidator {
+
+//	@Check
+//	public void checkTypeNameStartsWithCapital(Type type) {
+//		if (!Character.isUpperCase(type.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.TYPE__NAME);
+//		}
+//	}
+
+}
