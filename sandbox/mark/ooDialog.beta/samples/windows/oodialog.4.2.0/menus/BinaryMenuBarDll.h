0e90abda279947e57dd0eb0f545a94326c113c56
miesfeld <miesfeld@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2011-10-09 18:27:45 +0000

    sandbox ooDialog - syn with main up through r7188
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@7188 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- sandbox/mark/ooDialog.beta/samples/windows/oodialog.4.2.0/menus/BinaryMenuBarDll.h
+++
@@ -1,9 +0,0 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
-
-#define IDD_DIALOG1                             100
-#define IDB_BITMAP1                             102
-#define IDOK                                    1000
-#define IDCANCEL                                1001
-#define IDC_LV_LINES                            1003
