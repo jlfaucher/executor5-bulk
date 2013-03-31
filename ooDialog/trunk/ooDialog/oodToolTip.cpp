6dd45ff576cfea2cf187ee55574cbb94e5383b68
miesfeld <miesfeld@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2013-03-31 17:36:54 +0000

    ooDialog - fix possible problem in tool tip implementation
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@9157 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- ooDialog/trunk/ooDialog/oodToolTip.cpp
+++ ooDialog/trunk/ooDialog/oodToolTip.cpp
@@ -853,8 +853,11 @@ LRESULT CALLBACK ManageAtypicalToolProc(HWND hwnd, uint32_t msg, WPARAM wParam, 
                         result = TRUE;
                     }
 
-                    c->ReleaseLocalReference(rxToolID);
                     c->ReleaseLocalReference(args);
+                    if ( rxToolID != TheNilObj )
+                    {
+                        c->ReleaseLocalReference(rxToolID);
+                    }
 
                     return result;
                 }
@@ -871,7 +874,7 @@ LRESULT CALLBACK ManageAtypicalToolProc(HWND hwnd, uint32_t msg, WPARAM wParam, 
 
                     RexxDirectoryObject info = c->NewDirectory();
 
-                    RexxObjectPtr   rxToolID  = getToolIDFromLParam(c, lParam);
+                    RexxObjectPtr    rxToolID = getToolIDFromLParam(c, lParam);
                     RexxObjectPtr    userData = nmtdi->lParam == NULL ? TheNilObj : (RexxObjectPtr)nmtdi->lParam;
                     RexxStringObject flags    = ttdiFlags2keyword(c, nmtdi->uFlags);
 
@@ -934,9 +937,12 @@ LRESULT CALLBACK ManageAtypicalToolProc(HWND hwnd, uint32_t msg, WPARAM wParam, 
 
                     c->ReleaseLocalReference(_text);
                     c->ReleaseLocalReference(flags);
-                    c->ReleaseLocalReference(rxToolID);
                     c->ReleaseLocalReference(info);
                     c->ReleaseLocalReference(args);
+                    if ( rxToolID != TheNilObj )
+                    {
+                        c->ReleaseLocalReference(rxToolID);
+                    }
 
                     return TRUE;
                 }
@@ -955,8 +961,11 @@ LRESULT CALLBACK ManageAtypicalToolProc(HWND hwnd, uint32_t msg, WPARAM wParam, 
 
                     // ignore return.
 
-                    c->ReleaseLocalReference(rxToolID);
                     c->ReleaseLocalReference(args);
+                    if ( rxToolID != TheNilObj )
+                    {
+                        c->ReleaseLocalReference(rxToolID);
+                    }
 
                     return 0;
                 }
@@ -982,8 +991,11 @@ LRESULT CALLBACK ManageAtypicalToolProc(HWND hwnd, uint32_t msg, WPARAM wParam, 
                     }
 
                     c->ReleaseLocalReference(reply);
-                    c->ReleaseLocalReference(rxToolID);
                     c->ReleaseLocalReference(args);
+                    if ( rxToolID != TheNilObj )
+                    {
+                        c->ReleaseLocalReference(rxToolID);
+                    }
 
                     return 0;
                 }
