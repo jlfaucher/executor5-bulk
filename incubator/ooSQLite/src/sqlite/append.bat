66cb0d48b5f48e16605eabf8f61468978c488e5d
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2019-02-19 19:50:12 +0000

    update oosqlite with sqlite 3.27.1
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@11784 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- incubator/ooSQLite/src/sqlite/append.bat
+++ incubator/ooSQLite/src/sqlite/append.bat
@@ -1,2 +1,7 @@
+@echo off
+:-- download sqlite-amalgamation-nnnnnnn.zip from https://sqlite.org/download.html
+:-- unpack four files shell., sqlite3.c sqlite3.h, and sqlite3ext.h to this directory
+:-- then run this batch file
+:-- then in the base directory run nmake -fmakefile.win package
 copy sqlite3.c+sqlite3.c-append sqlite3.c
 copy sqlite3.h+sqlite3.h-append sqlite3.h
