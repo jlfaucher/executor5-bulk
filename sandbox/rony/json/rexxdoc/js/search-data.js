7c73125ee539df4b0a7cb7fb4c43f69dd8d2807e
orexx <orexx@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2021-06-06 15:39:58 +0000

    20210606 new sandbox, 'json.cls' enhanced, plug-in replacement.
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@12265 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

---
+++ sandbox/rony/json/rexxdoc/js/search-data.js
@@ -0,0 +1,65 @@
+var searchData = [
+        {
+        id: "json.cls", type: "Package", name: "json.cls", info: null,  href: "json.cls.html"
+    },
+        {
+        id: "JSON", type: "Class", name: "JSON", info: "json.cls", href: "json.cls.html#JSON"
+        },
+            {
+            id: "getEncoding", type: "Method", name: "getEncoding", info: "Class JSON", href: "json.cls.html#getEncoding"
+            },
+            {
+            id: "getEncodingDataOffset", type: "Method", name: "getEncodingDataOffset", info: "Class JSON", href: "json.cls.html#getEncodingDataOffset"
+            },
+            {
+            id: "getJsonBOM", type: "Method", name: "getJsonBOM", info: "Class JSON", href: "json.cls.html#getJsonBOM"
+            },
+            {
+            id: "fromJsonFile", type: "Method", name: "fromJsonFile", info: "Class JSON", href: "json.cls.html#fromJsonFile"
+            },
+            {
+            id: "toJsonFile", type: "Method", name: "toJsonFile", info: "Class JSON", href: "json.cls.html#toJsonFile"
+            },
+            {
+            id: "init", type: "Method", name: "init", info: "Class JSON", href: "json.cls.html#init"
+            },
+            {
+            id: "init-1", type: "Method", name: "init", info: "Class JSON", href: "json.cls.html#init-1"
+            },
+            {
+            id: "toJSON", type: "Method", name: "toJSON", info: "Class JSON", href: "json.cls.html#toJSON"
+            },
+            {
+            id: "fromJSON", type: "Method", name: "fromJSON", info: "Class JSON", href: "json.cls.html#fromJSON"
+            },
+            {
+            id: "minify", type: "Method", name: "minify", info: "Class JSON", href: "json.cls.html#minify"
+            },
+            {
+            id: "minify-1", type: "Method", name: "minify", info: "Class JSON", href: "json.cls.html#minify-1"
+            },
+            {
+            id: "toMinifiedJson", type: "Method", name: "toMinifiedJson", info: "Class JSON", href: "json.cls.html#toMinifiedJson"
+            },
+            {
+            id: "version", type: "Attribute", name: "version", info: "Class JSON", href: "json.cls.html#version"
+            },
+            {
+            id: "true", type: "Attribute", name: "true", info: "Class JSON", href: "json.cls.html#true"
+            },
+            {
+            id: "false", type: "Attribute", name: "false", info: "Class JSON", href: "json.cls.html#false"
+            },
+            {
+            id: "jsonDirClass", type: "Attribute", name: "jsonDirClass", info: "Class JSON", href: "json.cls.html#jsonDirClass"
+            },
+            {
+            id: "jsonDirClass-1", type: "Attribute", name: "jsonDirClass", info: "Class JSON", href: "json.cls.html#jsonDirClass-1"
+            },
+            {
+            id: "legible", type: "Attribute", name: "legible", info: "Class JSON", href: "json.cls.html#legible"
+            },
+            {
+            id: "legible-1", type: "Attribute", name: "legible", info: "Class JSON", href: "json.cls.html#legible-1"
+            },
+];
