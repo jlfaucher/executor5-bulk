jlfaucher/executor5-bulk

commit 3b6bf5bb98af5f333dfb9d6d153d6f15c1f491e2

fix code and tests for [bugs:#1666] NUMERIC DIGITS should reset to package default
git-svn-id: file:///local/rexx/oorexx/oorexx.svn@12530 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

main/trunk/interpreter/instructions/NumericInstruction.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2018 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2022 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -81,7 +81,14 @@ void RexxInstructionNumeric::execute(RexxActivation *context, ExpressionStack *s
         // no expression?  Just set digits back to default
         if (expression == OREF_NULL)
         {
-            context->setDigits(Numerics::DEFAULT_DIGITS);
+            wholenumber_t default = context->getPackage()->getDigits();
+            // digits cannot be less than or equal to fuzz
+            if (default <= context->fuzz())
+            {
+                reportException(Error_Expression_result_digits, default, context->fuzz());
+            }
+            // set the value
+            context->setDigits(default);
         }
         // expression version
         else
@@ -112,7 +119,14 @@ void RexxInstructionNumeric::execute(RexxActivation *context, ExpressionStack *s
         // no expression resets to default
         if (expression == OREF_NULL)
         {
-            context->setFuzz(Numerics::DEFAULT_FUZZ);
+            wholenumber_t default = context->getPackage()->getFuzz();
+            // cannot be greater than or equal to digits
+            if (default >= context->digits())
+            {
+                reportException(Error_Expression_result_digits, context->digits(), default);
+            }
+            // set the value
+            context->setFuzz(default);
         }
         else
         {
@@ -145,7 +159,7 @@ void RexxInstructionNumeric::execute(RexxActivation *context, ExpressionStack *s
             // if default form, set that
             if (numericFlags[numeric_form_default])
             {
-                context->setForm(Numerics::DEFAULT_FORM);
+                context->setForm(context->getPackage()->getForm());
             }
             else
             {

test/trunk/ooRexx/base/keyword/NUMERIC.testGroup
@@ -558,6 +558,7 @@ return testResult
    Numeric Form Value 'engineering'
 
 ::method "test_87"
+
    self~expectSyntax(25.11)
    Numeric Form Value 'scientific'
 
@@ -585,18 +586,6 @@ return testResult
    case=form()
    Return case
 
-::method "test_90"
-   str.='ENGINEERING'
-   Numeric Form Value str.
-   self~assertSame(case(), str.)
-   str.='SCIENTIFIC'
-   Numeric Form Value str.
-   self~assertSame(case(), str.)
-   return
-   case:
-   case=form()
-   Return case
-
 ::method "test_91"
    case=''
    str='ENGINEERINGSCIENTIFIC'
@@ -611,4 +600,108 @@ return testResult
    Return case
 
 
-::options novalue error
+::method test_numeric_nosymbol
+  -- NUMERIC must be followed by one of the keywords DIGITS, FORM, or FUZZ
+  self~assertSyntaxError(20.905, "numeric")
+
+::method test_numeric_nosymbol_string
+  -- NUMERIC must be followed by one of the keywords DIGITS, FORM, or FUZZ
+  self~assertSyntaxError(20.905, "numeric 'DIGITS'")
+
+::method test_numeric_symbol_invalid
+  -- NUMERIC must be followed by one of the keywords DIGITS, FORM, or FUZZ
+  self~assertSyntaxError(25.15, "numeric digi")
+
+::method test_numeric_symbol_number
+  -- NUMERIC must be followed by one of the keywords DIGITS, FORM, or FUZZ
+  self~assertSyntaxError(25.15, "numeric 9")
+
+::method test_digits_smaller_than_fuzz
+  numeric fuzz 8
+  self~expectSyntax((33.1, 7, 8)) -- Value of NUMERIC DIGITS ("7") must exceed value of NUMERIC FUZZ ("8")
+  numeric digits 7
+
+::method test_digits_equal_to_fuzz
+  numeric fuzz 5
+  self~expectSyntax((33.1, 5, 5)) -- Value of NUMERIC DIGITS ("5") must exceed value of NUMERIC FUZZ ("5")
+  numeric digits 5
+
+::method test_fuzz_larger_than_digits
+  self~expectSyntax((33.1, 9, 10)) -- Value of NUMERIC DIGITS ("9") must exceed value of NUMERIC FUZZ ("10")
+  numeric fuzz 10
+
+::method test_fuzz_equal_to_digits
+  self~expectSyntax((33.1, 9, 9)) -- Value of NUMERIC DIGITS ("9") must exceed value of NUMERIC FUZZ ("9")
+  numeric fuzz 9
+
+::method test_digits_default_smaller
+  numeric digits 18
+  numeric fuzz 12
+  self~expectSyntax((33.1, 9, 12)) -- Value of NUMERIC DIGITS ("9") must exceed value of NUMERIC FUZZ ("12")
+  numeric digits
+
+::method test_fuzz_default_larger
+  -- Value of NUMERIC DIGITS ("5") must exceed value of NUMERIC FUZZ ("5")
+  self~assertSyntaxError(33.1, -
+   "numeric fuzz 0; numeric digits 5; numeric fuzz; ::options fuzz 5")
+
+::method test_digits_fuzz_default
+  -- NUMERIC DIGITS without an expression should reset to package default
+  self~assertSame(10, self~runDynamicSource( -
+   "numeric digits 9; numeric digits; return digits(); ::options digits 10"))
+
+  -- NUMERIC FUZZ without an expression should reset to package default
+  self~assertSame(1, self~runDynamicSource( -
+   "numeric fuzz 2; numeric fuzz; return fuzz(); ::options fuzz 1"))
+
+::method test_fuzz_integer
+  a = 555555555
+  b = a
+  do f = 1 to 8
+    numeric fuzz f
+    self~assertTrue( a  = b, "fuzz" f a "=" b)
+    self~assertTrue( a <= b, "fuzz" f a "<=" b)
+    self~assertTrue( a >= b, "fuzz" f a "<=" b)
+    self~assertFalse(a \= b, "fuzz" f a "\=" b)
+    self~assertFalse(a  < b, "fuzz" f a "<" b)
+    self~assertFalse(a  > b, "fuzz" f a ">" b)
+    a = a % 10
+    b = b % 10 + 1
+  end
+
+::method test_fuzz
+  numeric fuzz 0
+  a = 123456789e2
+  b = 123456785e2
+  do 21
+    a /= 10; b /= 10
+    self~assertFalse(a  = b, "fuzz 0" a "=" b)
+    self~assertFalse(a <= b, "fuzz 0" a "<=" b)
+    self~assertTrue( a >= b, "fuzz 0" a "<=" b)
+    self~assertTrue( a \= b, "fuzz 0" a "\=" b)
+    self~assertFalse(a  < b, "fuzz 0" a "<" b)
+    self~assertTrue( a  > b, "fuzz 0" a ">" b)
+  end
+  do f = 1 to 2
+    numeric fuzz f
+    a = 123456789e2
+    b = 123456785e2
+    do 21
+      a /= 10; b /= 10
+      self~assertTrue( a  = b, "fuzz" f a "=" b)
+      self~assertTrue( a <= b, "fuzz" f a "<=" b)
+      self~assertTrue( a >= b, "fuzz" f a "<=" b)
+      self~assertFalse(a \= b, "fuzz" f a "\=" b)
+      self~assertFalse(a  < b, "fuzz" f a "<" b)
+      self~assertFalse(a  > b, "fuzz" f a ">" b)
+    end
+  end
+
+::method test_form_default
+  -- NUMERIC FORM without an expression should reset to package default
+  self~assertSame("ENGINEERING", self~runDynamicSource( -
+   "numeric form scientific; numeric form; return form(); ::options form engineering"))
+
+
+-- some legacy tests require novalue and lostdigits NOT to be raised
+::options all syntax novalue condition lostdigits condition

commit 104f3f6b2a13bcd8f2374fa20053686248e7d935

Fix compile problems caused by [r12530]
git-svn-id: file:///local/rexx/oorexx/oorexx.svn@12531 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

main/trunk/interpreter/instructions/NumericInstruction.cpp
@@ -81,14 +81,14 @@ void RexxInstructionNumeric::execute(RexxActivation *context, ExpressionStack *s
         // no expression?  Just set digits back to default
         if (expression == OREF_NULL)
         {
-            wholenumber_t default = context->getPackage()->getDigits();
+            wholenumber_t defaultDigits = context->getPackage()->getDigits();
             // digits cannot be less than or equal to fuzz
-            if (default <= context->fuzz())
+            if (defaultDigits <= context->fuzz())
             {
-                reportException(Error_Expression_result_digits, default, context->fuzz());
+                reportException(Error_Expression_result_digits, defaultDigits, context->fuzz());
             }
             // set the value
-            context->setDigits(default);
+            context->setDigits(defaultDigits);
         }
         // expression version
         else
@@ -119,14 +119,14 @@ void RexxInstructionNumeric::execute(RexxActivation *context, ExpressionStack *s
         // no expression resets to default
         if (expression == OREF_NULL)
         {
-            wholenumber_t default = context->getPackage()->getFuzz();
+            wholenumber_t defaultFuzz = context->getPackage()->getFuzz();
             // cannot be greater than or equal to digits
-            if (default >= context->digits())
+            if (defaultFuzz >= context->digits())
             {
-                reportException(Error_Expression_result_digits, context->digits(), default);
+                reportException(Error_Expression_result_digits, context->digits(), defaultFuzz);
             }
             // set the value
-            context->setFuzz(default);
+            context->setFuzz(defaultFuzz);
         }
         else
         {