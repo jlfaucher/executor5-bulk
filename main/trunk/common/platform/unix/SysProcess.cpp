3141fed9a2425aa1fb71727513f52ebb01436054
erich_st <erich_st@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2025-04-02 10:17:46 +0000

    fix code for [bugs:#2005] SysFileTree Segmentation Fault; also unrelated OpenBSD comment update
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@12953 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- main/trunk/common/platform/unix/SysProcess.cpp
+++ main/trunk/common/platform/unix/SysProcess.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2021 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2025 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -144,6 +144,7 @@ const char* SysProcess::getExecutableFullPath()
     }
 #elif defined HAVE_KERN_PROC_ARGV
     // OpenBSD
+    // https://github.com/ziglang/zig/issues/6718#issuecomment-711134120
     // no means to retrieve the executable path, need to resort to argv[0]
     int mib[4] = {CTL_KERN, KERN_PROC_ARGS, getpid(), KERN_PROC_ARGV};
     size_t len;
