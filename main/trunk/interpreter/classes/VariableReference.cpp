6f9f0d400d1fc15b90b08daf8e665d6406c19e66
jmblasc2 <jmblasc2@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2026-01-27 10:18:50 +0000

    Fix for [Bug:#2046]
    git-svn-id: file:///local/rexx/oorexx/oorexx.svn@13084 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- main/trunk/interpreter/classes/VariableReference.cpp
+++ main/trunk/interpreter/classes/VariableReference.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /* Copyright (c) 1995, 2004 IBM Corporation. All rights reserved.             */
-/* Copyright (c) 2005-2018 Rexx Language Association. All rights reserved.    */
+/* Copyright (c) 2005-2026 Rexx Language Association. All rights reserved.    */
 /*                                                                            */
 /* This program and the accompanying materials are made available under       */
 /* the terms of the Common Public License v1.0 which accompanies this         */
@@ -135,8 +135,8 @@ void VariableReference::flatten (Envelope *envelope)
 {
     setUpFlatten(VariableReference)
 
-    memory_mark_general(objectVariables);
-    memory_mark_general(variable);
+    flattenRef(objectVariables);
+    flattenRef(variable);
 
     cleanUpFlatten
 }
